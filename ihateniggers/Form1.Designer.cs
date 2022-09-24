1962c02bcb071dc6b0957abfc9a58cab76ad10b8
Igor <Igor@PRODATA>
2022-09-24 19:50:09 +0200

    Dodaj pliki projektów.

---
+++ ihateniggers/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace ihateniggers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.clicksLbl = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timerLbl = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.najLbl = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pktLbl = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Klikniecia: ";
+            // 
+            // clicksLbl
+            // 
+            this.clicksLbl.AutoSize = true;
+            this.clicksLbl.Location = new System.Drawing.Point(81, 10);
+            this.clicksLbl.Name = "clicksLbl";
+            this.clicksLbl.Size = new System.Drawing.Size(13, 15);
+            this.clicksLbl.TabIndex = 1;
+            this.clicksLbl.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Timer: ";
+            // 
+            // timerLbl
+            // 
+            this.timerLbl.AutoSize = true;
+            this.timerLbl.Location = new System.Drawing.Point(81, 37);
+            this.timerLbl.Name = "timerLbl";
+            this.timerLbl.Size = new System.Drawing.Size(13, 15);
+            this.timerLbl.TabIndex = 3;
+            this.timerLbl.Text = "0";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 149);
+            this.progressBar.Maximum = 1000;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(144, 23);
+            this.progressBar.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 131);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "UZBIERAJ 1000 PUNKTOW";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Klik";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.clickerBtn_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 15);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Najlepszy:";
+            // 
+            // najLbl
+            // 
+            this.najLbl.AutoSize = true;
+            this.najLbl.Location = new System.Drawing.Point(81, 81);
+            this.najLbl.Name = "najLbl";
+            this.najLbl.Size = new System.Drawing.Size(13, 15);
+            this.najLbl.TabIndex = 8;
+            this.najLbl.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 106);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Punkty:";
+            // 
+            // pktLbl
+            // 
+            this.pktLbl.AutoSize = true;
+            this.pktLbl.Location = new System.Drawing.Point(81, 106);
+            this.pktLbl.Name = "pktLbl";
+            this.pktLbl.Size = new System.Drawing.Size(13, 15);
+            this.pktLbl.TabIndex = 10;
+            this.pktLbl.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(371, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(59, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Kup";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(219, 59);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(146, 15);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "klikniecia +1 - 50 punktow";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 184);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pktLbl);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.najLbl);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.timerLbl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.clicksLbl);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label clicksLbl;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label timerLbl;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label najLbl;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label pktLbl;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ ihateniggers/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ihateniggers
+{
+    public partial class Form1 : Form
+    {
+        public int klikniecia = 0;
+        public int klikanieNaliczajacePunkty = 1;
+        public int punktyDoZbierania = 0;
+        public int NajwiecejPunktowPodczasJednejPruby = 0;
+        public int CzasPodejscia = 5;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clickerBtn_Click(object sender, EventArgs e)
+        {
+            if (klikniecia == 0) StartCzasuIProby();
+            klikniecia += klikanieNaliczajacePunkty;
+            clicksLbl.Text = "" + klikniecia;
+        }
+        
+        public async void StartCzasuIProby()
+        {
+            CzasPodejscia = 5;
+            for (int i = 0; i < 5; i++)
+            {
+
+                timerLbl.Text = "" + CzasPodejscia;
+                await Task.Delay(1000);
+                CzasPodejscia--;
+            }
+            timerLbl.Text = "" + CzasPodejscia;
+            if (klikniecia > NajwiecejPunktowPodczasJednejPruby) NajwiecejPunktowPodczasJednejPruby = klikniecia;
+            najLbl.Text = "" + NajwiecejPunktowPodczasJednejPruby;
+            punktyDoZbierania += klikniecia;
+            pktLbl.Text = "" + punktyDoZbierania;
+            klikniecia = 0;
+            if (punktyDoZbierania <= 1000) progressBar.Value = punktyDoZbierania;
+            else
+            {
+                progressBar.Value = 1000;
+                MessageBox.Show("WYGRANA ! ! !");
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (punktyDoZbierania >= 50)
+            {
+                klikanieNaliczajacePunkty += 1;
+                punktyDoZbierania = punktyDoZbierania - 50;
+                pktLbl.Text = "" + punktyDoZbierania;
+            }
+        }
+    }
+}
